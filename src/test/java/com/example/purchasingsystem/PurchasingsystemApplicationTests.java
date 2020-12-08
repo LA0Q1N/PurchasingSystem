121a456aba8722456eda8b07319c8b1ea5b4f4c8
QinFei <86137123@qq.com>
2020-12-08 12:50:22 +0800

    first commit

---
+++ src/test/java/com/example/purchasingsystem/PurchasingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.purchasingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchasingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
